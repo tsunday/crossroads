906a840985c26da31cdcae3684d79a9c20a3eebf
tsunday <tomekniedz@gmail.com>
2013-12-02 17:48:01 +0100

    cw3

--- src/crossword/Test.java
+++ src/crossword/Test.java
@@ -10,7 +10,6 @@ public class Test {
 		String in = "c:\\Users\\Tomek\\Programowanie\\java\\crossroads\\cwdb.txt";
 		String out = "c:\\Users\\Tomek\\Programowanie\\java\\crossroads\\cwdb_out.txt";
 		InteliCwDB db = new InteliCwDB(in);
-		Crossword cw = new Crossword(10,10,db);
 		
 		try{
 			db.saveDB(out);
--- src/dictionary/Strategy.java
+++
@@ -1,6 +0,0 @@
-package dictionary;
-
-public abstract class Strategy {
-	public abstract CwEntry findEntry(Crossword cw);
-	public abstract void updateBoard(Board b, CwEntry e); 
-}
